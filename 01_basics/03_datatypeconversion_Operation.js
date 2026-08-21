c43480a1cef03f1a05015ba8e9ce6283cc483104
Sejalgaonkar33 <sejalgaonkar33@gmail.com>
2026-08-21 09:25:48 +0000

    conversion

---
+++ 01_basics/03_datatypeconversion_Operation.js
@@ -0,0 +1,58 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Sejal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Sejal" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************OPERATIONS*************************** //
+
+value = 33
+
+let negvalue = -value
+// console.log(negvalue);
+
+//console.log(2 + 2);
+//console.log(2 - 2);
+//console.log(2 * 2);
+//console.log(2 ** 2);
+//console.log(2 / 3);
+//console.log(2 % 3);
+
+let str1 = "Hello"
+let str2 = "Sejal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log( "1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + 2 + 2);
+// console.log( 1 + 2 + "2");
+
+// console.log( (3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
