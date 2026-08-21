197661f49b8a50eca50edb358ab9bf9d5c337c92
Sejalgaonkar33 <sejalgaonkar33@gmail.com>
2026-08-21 06:31:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code read bility should be high
+
+console.log("Sejal")
+
+let name = "Sejal"
+let age = 17
+let isLoggedIn = false 
+let state;
+
+// number => 2 o power 53
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined =>
+// sumbol => unique
+
+// object
+
+console.log(typeof "Sejal")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null) // undefined
+console.log(typeof undefined) // object
+
+
+
+
